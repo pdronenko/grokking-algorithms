46e4308697e2c698d9ad95470184257b20c8b5f7
pdronenko <pdronenko@ya.ru>
2019-07-05 10:26:59 +0300

    add binary search test

--- __tests__/solutions.test.js
+++ __tests__/solutions.test.js
@@ -1,5 +1,8 @@
-import half from '../solutions';
+import binarySearch from '../solutions';
 
-test('half', () => {
-  expect(half(6)).toBe(3);
+test('binarySearch', () => {
+  const list = [2, 5, 7, 12, 23, 78, 145, 523];
+  expect(binarySearch(list, 23)).toBe(4);
+  expect(binarySearch(list, 523)).toBe(7);
+  expect(binarySearch(list, 52)).toBeNull();
 });
